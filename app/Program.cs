ea2b7e7ae0c6e885351e45f55ee63249de7326b9
Tim <tim.bjorkegren@gmail.com>
2024-11-20 15:17:01 +0100

    Add user input and guess selection
    currently works for one guess need a gameloop and some system for
    displaying the outpout.

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+string[] possible_words = ["new", "banana", "apple", "cool"];
+Random get_random = new();
+
+string solution = possible_words[get_random.Next(possible_words.Length)];
+
+char guess;
+string? input;
+
+do
+{
+    Console.WriteLine("Please enter a character to guess with");
+    input = Console.ReadLine();
+    Debug.Assert(input != null);
+} while (input.Length == 1);
+
+bool guess_was_correct = false;
+
+foreach (char c in solution)
+{
+    if (guess == c)
+    {
+        guess_was_correct = true;
+    }
+}
+
+if (guess_was_correct)
+{
+    Console.WriteLine($"{guess} exists in the word");
+}
+else
+{
+    Console.WriteLine($"{guess} does not exist in the word");
+}
+
+
+
+
+
