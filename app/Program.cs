66cc1fa48d42db6db8d6d47f7e4fcd750b80a8c4
Tim <tim.bjorkegren@gmail.com>
2024-11-21 12:47:52 +0100

    changed some variable
    changed the guess from a char because it was trouble to use it as a char
    and used instead guess as a string, need to fix so everything goes hand
    in ha

--- app/Program.cs
+++ app/Program.cs
@@ -2,25 +2,31 @@
 
 string[] possible_words = ["new", "banana", "apple", "cool"];
 Random get_random = new();
-
 string solution = possible_words[get_random.Next(possible_words.Length)];
+string guess = get_user_guess();
 
-char guess;
-string? input;
+string display = new('_', solution.Length);
+Console.WriteLine(display);
 
 do
 {
     Console.WriteLine("Please enter a character to guess with");
-    input = Console.ReadLine();
+    string? input = Console.ReadLine();
     Debug.Assert(input != null);
-} while (input.Length == 1);
+    if (input.Length == 1)
+    {
+        guess = input[0];
+    }
+} while (guess == null);
 
 bool guess_was_correct = false;
 
-foreach (char c in solution)
+for (int i = 0; i < solution.Length; i += 1)
 {
-    if (guess == c)
+    if (guess == solution[i])
     {
+        display.Remove(i, 1);
+        display.Insert(i,guess);
         guess_was_correct = true;
     }
 }
@@ -34,7 +40,21 @@ else
     Console.WriteLine($"{guess} does not exist in the word");
 }
 
-
+char get_user_guess()
+{
+    string? guess == null;
+    do
+    {
+        Console.WriteLine("Please enter a character to guess with");
+        string? input = Console.ReadLine();
+        Debug.Assert(input != null);
+        if (input.Length == 1)
+        {
+            guess = input[0];
+        }
+    } while (guess == null);
+    return guess;
+}
 
 
 
